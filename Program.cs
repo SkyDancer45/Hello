bafa040e1da1407bbc26277f9132af9fb676f723
Kanishk Purohit <kishupurohit1@gmail.com>
2024-05-28 01:29:53 +0530

    stage2

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using GameStore.Api.Dtos;
 //using System;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-List<GameDto> games = [new(1, "COD", "Action", 50.0M, new DateOnly(2010, 09, 30))];
+List<GameDto> games = [new(1, "COD", "Action", 50.0M, new DateOnly(2010, 09, 30)), new(2, "Witcher 3", "Fantasy", 52.0M, new DateOnly(2010, 09, 29))];
 app.MapGet("games", () => games[0].Name);
 
 app.MapGet("/", () => "Hellosasadasdda World!");
