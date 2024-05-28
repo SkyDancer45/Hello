bf80aa70b2e06e6a97306f598a0e51176e941037
Kanishk Purohit <kishupurohit1@gmail.com>
2024-05-28 12:28:22 +0530

    this is as is stands

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ var app = builder.Build();
 List<GameDto> games = [new(1, "COD", "Action", 50.0M, new DateOnly(2010, 09, 30)), new(2, "Witcher 3", "Fantasy", 52.0M, new DateOnly(2010, 09, 29))];
 app.MapGet("games", () => games[0].Name);
 
-app.MapGet("/", () => "Hellosasadasdda World!");
+// app.MapGet("/", () => "Hellosasadasdda World!");
 Console.Write("Mooo");
+//GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapPost("games",(CreateGameDtos newGame)=>{
+
+});
 app.Run();
